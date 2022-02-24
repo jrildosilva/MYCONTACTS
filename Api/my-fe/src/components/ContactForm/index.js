9ccbe67456c80180b5741a1b526669e152f8a91c
jrildosilva <jrsrildo44@gmail.com>
2022-02-24 17:48:04 -0300

    validando o campo name

--- Api/my-fe/src/components/ContactForm/index.js
+++ Api/my-fe/src/components/ContactForm/index.js
@@ -13,6 +13,24 @@ export default function ContactForm({ buttonLabel }) {
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [category, setCategory] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setErrors((prevState) => [
+        ...prevState,
+        {field: 'name', message: 'nome é obrigatório'},
+
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter(
+        (error) => error.field !== 'name',
+      ));
+    }
+  }
+  console.log(errors);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -28,7 +46,7 @@ export default function ContactForm({ buttonLabel }) {
         <Input
           placeholder="Nome"
           value={name}
-          onChange={(event) => setName(event.target.value)}
+          onChange={handleNameChange}
         />
       </FormGroup>
 
