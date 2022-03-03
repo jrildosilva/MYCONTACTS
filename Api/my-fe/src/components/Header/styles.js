03373c57fffb5fad4e7e5141411c26f7afd5114d
jrildosilva <jrsrildo44@gmail.com>
2022-03-03 16:03:28 -0300

    cont

--- Api/my-fe/src/components/Header/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.header`
-  margin-top: 74px;
-  margin-bottom: 48px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
