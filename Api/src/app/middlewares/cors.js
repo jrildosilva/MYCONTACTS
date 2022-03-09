4f296ea2388c37d3fc568c79f867a11f881a5388
jrildosilva <jrsrildo44@gmail.com>
2022-03-09 19:03:05 -0300

    cont.

--- Api/src/app/middlewares/cors.js
+++ Api/src/app/middlewares/cors.js
@@ -1,5 +1,6 @@
-module.exports = ((request, response, next) => {
+module.exports = (request, response, next) => {
   response.setHeader('Access-control-Allow-Origin', 'http://localhost:3000');
   response.setHeader('Access-control-Allow-Methods', '*');
+  response.setHeader('Access-control-Allow-Methods', '*');
   next();
-});
+};
