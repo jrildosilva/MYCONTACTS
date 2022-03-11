d6300e4133998286cbd97b34e19d842468bcdd28
jrildosilva <jrsrildo44@gmail.com>
2022-03-11 19:04:32 -0300

    renderizando o API

--- my-fe/src/pages/Home/index.js
+++ my-fe/src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import {
-  Container, InputSearchContainer, Header, ListContainer, Card,
+  Container, InputSearchContainer, Header, ListHeader, Card,
 } from './styles';
 
 import arrow from '../../assets/images/icons/arrow.svg';
@@ -10,22 +10,24 @@ import trash from '../../assets/images/icons/trash.svg';
 
 export default function Home() {
   const [contacts, setContacts] = useState([]);
+  const [orderBy, setOrderBy] = useState('asc');
 
   useEffect(() => {
-    fetch('http://localhost:3001/contacts')
+    fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
       .then(async (response) => {
         const json = await response.json();
         setContacts(json);
-
-        setContacts([{}]);
       })
-
       .catch((error) => {
         console.log('erro', error);
       });
-  }, []);
+  }, [orderBy]);
 
-  console.log(contacts);
+  function handleToggleOrderBy() {
+    setOrderBy(
+      (prevState) => (prevState === 'asc' ? 'desc' : 'asc'),
+    );
+  }
 
   return (
     <Container>
@@ -37,19 +39,16 @@ export default function Home() {
          <strong>
            {contacts.length}
            {contacts.length === 1 ? ' Contato ' : ' Contatos '}
-          contatos
          </strong>
          <Link to="/new">Novo contato</Link>
       </Header>
 
-      <ListContainer>
-        <header>
-          <button type="button">
+      <ListHeader orderBy={orderBy}>
+          <button type="button" onClick={handleToggleOrderBy}>
              <span>Nome</span>
              <img src={arrow} alt="Arrow" />
           </button>
-        </header>
-      </ListContainer>
+      </ListHeader>
 
        {contacts.map((contact) => (
           <Card key={contact.id}>
@@ -64,14 +63,14 @@ export default function Home() {
              <span>{contact.phone}</span>
           </div>
 
-          <div className="actions">
+           <div className="actions">
              <Link to={`/edit/${contact.id}`}>
                <img src={edit} alt="Edit" />
              </Link>
              <button type="button">
                 <img src={trash} alt="Delete" />
              </button>
-          </div>
+           </div>
           </Card>
        ))}
 
--- my-fe/src/pages/Home/styles.js
+++ my-fe/src/pages/Home/styles.js
@@ -31,27 +31,27 @@ export const Header = styled.header`
   }
 `;
 
-export const ListContainer = styled.div`
-margin-top: 24px;
+export const ListHeader = styled.header`
+     margin-top: 24px;
+     margin-bottom: 8px;
 
-    header {
-       margin-bottom: 8px;
-
-       button {
+     button {
          background: transparent;
          border: none;
          display: flex;
          align-items: center;
 
-
-      span {
-        margin-right: 8px;
-        font-weight: bold;
-        color: ${({ theme }) => theme.colors.primary.main};
+       span {
+          margin-right: 8px;
+          font-weight: bold;
+          color: ${({ theme }) => theme.colors.primary.main};
+        }
+        img {
+        transform: ${({ orderBy }) => (orderBy === 'asc' ? 'rotate(180deg)' : 'rotate(0deg)')};
+        transition: transform 0.2s ease-in;
+        }
       }
-    }
-  }
-`;
+     `;
 export const InputSearchContainer = styled.div`
   width: 100%;
 
