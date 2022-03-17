715f1f03de63b0d472520b31c01e9d6cd51d1a73
jrildosilva <jrsrildo44@gmail.com>
2022-03-16 21:13:01 -0300

    busca de contatos

--- my-fe/src/pages/Home/index.js
+++ my-fe/src/pages/Home/index.js
@@ -13,6 +13,10 @@ export default function Home() {
   const [orderBy, setOrderBy] = useState('asc');
   const [searchTerm, setSearchTerm] = useState('');
 
+  const filteredContacts = contacts.filter((contact) => (
+    contact.name.toLowerCase().includes(searchTerm.toLowerCase())
+  ));
+
   useEffect(() => {
     fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
       .then(async (response) => {
@@ -23,18 +27,15 @@ export default function Home() {
         console.log('erro', error);
       });
   }, [orderBy]);
-  console.log(contacts);
+
   function handleToggleOrderBy() {
     setOrderBy(
       (prevState) => (prevState === 'asc' ? 'desc' : 'asc'),
     );
   }
-
   function handleChangeSearchTerm(event) {
     setSearchTerm(event.target.value);
   }
-
-  console.log(searchTerm);
 
   return (
     <Container>
@@ -49,20 +50,22 @@ export default function Home() {
 
       <Header>
          <strong>
-           {contacts.length}
-           {contacts.length === 1 ? ' Contato ' : ' Contatos '}
+           {filteredContacts.length}
+           {filteredContacts.length === 1 ? ' Contato ' : ' Contatos '}
          </strong>
          <Link to="/new">Novo contato</Link>
       </Header>
 
-      <ListHeader orderBy={orderBy}>
-          <button type="button" onClick={handleToggleOrderBy}>
-             <span>Nome</span>
-             <img src={arrow} alt="Arrow" />
-          </button>
-      </ListHeader>
+      {filteredContacts.length > 0 && (
+        <ListHeader orderBy={orderBy}>
+         <button type="button" onClick={handleToggleOrderBy}>
+           <span>Nome</span>
+           <img src={arrow} alt="Arrow" />
+         </button>
+        </ListHeader>
+      )}
 
-      {contacts.map((contact) => (
+      {filteredContacts.map((contact) => (
           <Card key={contact.id}>
              <div className="info">
                 <div className="contact-name">
@@ -82,7 +85,7 @@ export default function Home() {
               </button>
              </div>
           </Card>
-       ))}
+      ))}
 
     </Container>
   );
