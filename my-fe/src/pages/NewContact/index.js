57ee8917dd218e26a2e3f05cb24247d3f5fc2dfe
jrildosilva <jrsrildo44@gmail.com>
2022-05-27 20:11:02 -0300

    cadastro cont

--- my-fe/src/pages/NewContact/index.js
+++ my-fe/src/pages/NewContact/index.js
@@ -4,17 +4,22 @@ import ContactsService from '../../services/ContactsService';
 
 export default function NewContact() {
   async function handleSubmit(formData) {
-    const contact = {
-      name: formData.name,
-      email: formData.email,
-      phone: formData.phone,
-      category_id: formData.category_id,
-    };
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.category_id,
+      };
 
-    const response = await ContactsService.createContact(contact);
+      const response = await ContactsService.createContact(contact);
 
-    console.log(response);
+      console.log(response);
+    } catch {
+      alert('Ocorreu um erro ao cadastrar o contato!');
+    }
   }
+
   return (
    <>
        <PageHeader
